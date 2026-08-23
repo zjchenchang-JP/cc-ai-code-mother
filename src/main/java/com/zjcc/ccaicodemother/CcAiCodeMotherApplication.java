42b757dab1ed5b8b71b8855b345b4610bea37cc2
zjchenchang-JP <zjchenchang@gmail.com>
2026-08-23 10:14:49 +0900

    feat(user):引入Mybatis Flex框架

--- src/main/java/com/zjcc/ccaicodemother/CcAiCodeMotherApplication.java
+++ src/main/java/com/zjcc/ccaicodemother/CcAiCodeMotherApplication.java
@@ -1,11 +1,13 @@
 package com.zjcc.ccaicodemother;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(exposeProxy = true) // 可以通过 AopContext.currentProxy() 获取当前的代理对象
+@MapperScan("com.zjcc.ccaicodemother.mapper")
 public class CcAiCodeMotherApplication {
 
     public static void main(String[] args) {
